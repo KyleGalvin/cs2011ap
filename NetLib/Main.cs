90c30a6b3850f3bac21b8d3f9f7ee22e2565e4ff
DarkDigitalDream <DarkDigitalDream@85d7b1b0-ebf7-b8ab-b9b4-e9f080a193fb>
2011-01-30 23:37:52 +0000

    implementing communication protocols

--- NetLib/Main.cs
+++ NetLib/Main.cs
@@ -33,9 +33,16 @@ namespace NetTest
 			
 			while (true)
 			{
-				//wait for new connection and ad it to the end of our clients list
+				//wait for new incoming connection
 				client = tcpListener.AcceptTcpClient();
-				myConnections.Add(client);
+				
+				//We cannot be sending out on our connections while we add a new one
+				//since the list length cannot change while we iterate through the list
+				lock(this)
+				{
+					myConnections.Add(client);
+				}
+				
 				Console.WriteLine("Starting the {0}th connection",myConnections.Count);
 				//create a thread to handle communication
 				Thread clientThread = new Thread(new ParameterizedThreadStart(HandleIncomingComm));
@@ -60,15 +67,23 @@ namespace NetTest
 			listenThread.Start();
 		}
 		
-		private void Respond(byte[] data,Object client)
+		private void Respond(UInt32 data)
 		{
-			TcpClient tcpClient = (TcpClient)client;
+			Console.WriteLine("Responding to incoming communication...");
+			Console.WriteLine("Locking Connections resource - incoming connections must wait");
 			
+			byte[] rawData = BitConverter.GetBytes(data);//break down into bytes to send over socket
 			
-			//we will eventually need to do something other than echo back.
-			Console.WriteLine("sending response: {0} to client: {1}",data,tcpClient.Client.RemoteEndPoint);
-			int val = tcpClient.Client.Send(data);
-			Console.WriteLine("quepasa{0}",val);
+			lock(this)
+			{
+				foreach(TcpClient client in myConnections)
+				{
+					//we will eventually need to do something other than echo back.
+					Console.WriteLine("echoing response: {1}",data,client.Client.RemoteEndPoint);
+					int val = client.Client.Send(rawData);
+				}
+			}
+			Console.WriteLine("Freeing resource lock - now accepting incomming connections");
 		}
 		
 		protected void HandleOutgoingComm(object client)
@@ -82,21 +97,36 @@ namespace NetTest
 			NetworkStream clientStream = tcpClient.GetStream();
 			Console.WriteLine("client {0} has connected.", tcpClient.Client.RemoteEndPoint);
 				
-			byte[] message = new byte[4];
+			byte[] rawMessage = new byte[4];
+			UInt32 message;
 			
-			List<byte[]> data = new List<byte[]>();
+			List<UInt32> data = new List<UInt32>();
+			
+			//The current 32 bit data unit coming in
 			int bytesRead;
+			
+			//the header data unit indicates how many data units are expected to follow. This value includes the header data
+			UInt32 packageSize;
+			
+			System.Text.UTF8Encoding  encoding=new System.Text.UTF8Encoding();
+			
+			bytesRead = 0;
 			
 			while (true)
 			{
-				bytesRead = 0;
+				
 			
 			    try
 			    {
 					//blocks until a client sends a message
-					bytesRead = clientStream.Read(message, 0, 4);
+					bytesRead += clientStream.Read(rawMessage, 0, 4);
+					message = BitConverter.ToUInt32(rawMessage,0);
 					data.Add(message);
-					Respond(message,client);
+					
+					//the number of 32 bit reads to follow plus the 32 bit header
+					packageSize = GetExpectedPackageSize(message);
+					
+
 			    }
 			    catch
 			    {
@@ -104,19 +134,64 @@ namespace NetTest
 					break;
 			    }
 			
-			    if (bytesRead == 0)
+			    if (bytesRead == 0)//nothing was read from socket
 				{
+					Console.WriteLine("Client IP: {0}, Client Data: {1}",tcpClient.Client.RemoteEndPoint,encoding.GetString(rawMessage));
+					Respond(message);
 					//the client has disconnected from the server
 					Console.WriteLine("Client {0} has disconnected.",tcpClient.Client.RemoteEndPoint);
 					break;
 				}
+				if(bytesRead == packageSize)//we've accumulated the amount of data our header predicts
+				{
+					Console.WriteLine("Now we are able to test if we are picking up the right data here");
+					bytesRead = 0;
+				}
 				
-				//we read 32 bits at a time. This is a single float, a Uint32, or 4 chars
-				System.Text.UTF8Encoding  encoding=new System.Text.UTF8Encoding();
-				Console.WriteLine("Client IP: {0}, Client Data: {1}",tcpClient.Client.RemoteEndPoint,encoding.GetString(message));
 			}
 			
 			tcpClient.Close();
+		}
+		
+		private UInt32 GetExpectedPackageSize(UInt32 header)
+		{
+			//byte action = ((byte)header[0] >> 4);
+			UInt32 type = header & 0x000F;
+			UInt32 typesize=0;
+			UInt32 count = (header & 0x0F00) >> 8;//take only our count byte
+			
+			switch (type)
+			{
+			case 0x0://player
+				Console.WriteLine("00 = client player; 8 floats");
+				typesize=8;
+				break;
+			case 0x1://AI
+				Console.WriteLine("01 = enemy AI; 8 floats");
+				typesize=8;
+				break;
+			case 0x2://building
+				Console.WriteLine("02 = building; 8 floats");
+				typesize=8;
+				break;
+			case 0x4://bullet
+				Console.WriteLine("03 = bullet; 7 floats");
+				typesize=7;
+				break;
+			case 0x5://explosion
+				Console.WriteLine("04 = explosion; 8 floats");
+				typesize=8;
+				break;
+			case 0x6://power-up
+				Console.WriteLine("05 = power-up; 8 floats");
+				typesize=8;
+				break;
+			default:
+				Console.WriteLine("Invalid Command!");
+				break;
+			}
+			
+			return typesize*count;
 		}
 		
 	}
